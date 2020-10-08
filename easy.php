28c96c5586f43482fa32156b7009042f6dc7f5dd
astratow <davno_pora@mail.com>
2020-10-08 10:12:30 +0100

    php files for mysql and php in easy steps

---
+++ easy.php
@@ -0,0 +1,9 @@
+<?php
+$host='localhost:3306';
+$user='mike';
+$pass='easysteps';
+$db='site_db';
+$dbc=mysqli_connect($host, $user, $pass, $db) OR die(mysqli_connect_error());
+mysqli_set_charset($dbc, 'utf8');
+
+?>
---
+++ require.php
@@ -0,0 +1,6 @@
+<?php
+require('./connectDB.php');
+if(mysqli_ping($dbc)){
+  echo 'MySQL Server' . mysqli_get_server_info($dbc) . 'connected on ' . mysqli_get_host_info($dbc);
+}
+?>
