3635f2a908f96e6eb50561007175c1d7910f6a50
astratow <davno_pora@mail.com>
2020-10-12 05:08:44 +0100

    logic.php created

---
+++ logic.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title> Basic PHP template - Logic </title>
+  </head>
+  <body>
+    <?php
+      #logic manipulations
+      echo '<h1>Basic PHP template - Logic </h1>';
+      $yes = TRUE;
+      $no = FALSE;
+      $result=($no&&$no)?'TRUE':'FALSE';
+        echo "No AND No returns $result <br>";
+      $result=($yes&&$no)?'TRUE':'FALSE';
+        echo "Yes AND No returns $result <br>";
+      $result=($yes&&$yes)?'TRUE':'FALSE';
+        echo "Yes AND Yes returns $result <br>";
+       $result=($no||$no)?'TRUE':'FALSE';
+        echo "No OR No returns $result <br>";
+      $result=($yes||$no)?'TRUE':'FALSE';
+        echo "Yes OR No returns $result <br>";
+      $result=($yes||$yes)?'TRUE':'FALSE';
+        echo "Yes OR Yes returns $result <br>";
+ 
+    ?>
+
+  </body>
+</html>
