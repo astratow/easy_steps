0ecc1059447e1f2fc67a1e6c1c6dc9720059af5e
astratow <davno_pora@mail.com>
2020-10-15 04:33:01 +0100

    ifelse.php finished

--- ifelse.php
+++ ifelse.php
@@ -9,13 +9,32 @@
       #write traditional greatings;
       echo '<h1> Basic PHP template - if else statement</h1>';
       echo "<strong>if(4>2)<br>";
+      
       if(4>2){
-        echo '<p>Yes, 4 is greater than 2.</p><br>';
+        echo '<p>Yes, 4 is greater than 2.<br><br>';
         }
       echo "if((4>2)&&(8>4))<br><br>";
+
       if((4>2)&&(8>4)){
-        echo '4 is greater than 2 AND 8 is greater than 4.</strong><br><br>';
+      echo '4 is greater than 2 AND 8 is greater than 4.<br><br>';
       }
+
+      if(4>8){
+        echo '4 is greater than 8<br><br>';
+      }else{
+        echo '4 is less than 8<br><br>';
+      }
+      
+      if(4>8){
+        echo 'This test is TRUE</p>';
+      }elseif(8>4){
+        echo 'Alternative test is TRUE</p>';
+      }else{
+        echo 'Both test are FALSE</p></strong>';
+      }
+
+
+
    ?>
 
   </body>
