c4183096cc1610b7565cd1d0afb19a91b1d64b6a
astratow <davno_pora@mail.com>
2020-10-16 04:46:29 +0100

    forloop.php created. For loop tested

---
+++ whileloop.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title> Basic PHP template - While Loops </title>
+  </head>
+  <body>
+    <?php
+      #write traditional greatings;
+      echo '<h1>While Loops </h1>';
+      $numbers = array(10, 20, 30, 40, 50);
+      echo  '<dt><h4>While Loop:</h4>';
+      $i=0;
+      while($i<5){
+        echo "<dd>Element $i = $numbers[$i]";
+        $i++;
+      }
+      echo '<dt> <h4>Do While Loop</h4>';
+      $i=0;
+      do
+      {
+        echo "<dd>Element $i=$numbers[$i]";
+        $i++;
+      }
+      while($i<5);
+    ?>
+
+  </body>
+</html>
