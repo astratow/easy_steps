b4282c0d177e6dccff773044549af0c4b44ba87f
astratow <davno_pora@mail.com>
2020-10-16 05:30:42 +0100

    breakcontinue.php edited. Break and continue blocks added

--- breakcontinue.php
+++ breakcontinue.php
@@ -10,6 +10,12 @@
       echo '<h1>Breaking from loops </h1>';
       for($i=1; $i<4; $i++){
         for($j=1; $j<4; $j++){
+          if($i==1 && $j==1){
+            echo "<p>Continues inner loop  when i=$i and j=$j</p>";
+          }
+          if($i==2 && $j==1){
+            echo "<h3> Breaks inner loop when i=$i and j=$j</h3>";
+          }
           echo "<h4>Running i=$i and j=$j</h4>";
           }
       }
