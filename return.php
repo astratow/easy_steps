73babb7c0209d1a948706a8ea862f2d0af32c40f
astratow <davno_pora@mail.com>
2020-10-17 23:51:18 +0100

    return.php created. Work on table to be continued.

---
+++ return.php
@@ -0,0 +1,87 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title> Basic PHP - Returning Values </title>
+  </head>
+  <body>
+    <?php
+      #write traditional greetings;
+      echo '<h1>Returning Values</h1>';
+      #row header
+      echo "<table><thead><tr><th>Argument</th>
+                              <th>Description</th>
+                              <th>Example</th></thead>";
+      #row 1
+      echo "<tbody><tr><td align='center'>Y</td> 
+            <td align='center'>Year as four digits</td>
+            <td align='center'>2020</></tr>";
+      #row2 
+      echo "<tbody><tr><td align='center'>y</td> 
+            <td align='center'>Year as two digits</td>
+            <td align='center'>20</></tr>";
+        #row 3
+      echo "<tbody><tr><td align='center'>n</td> 
+            <td align='center'>Month as one or two digits</td>
+            <td align='center'>5</></tr>";
+     
+      #row 4
+      echo "<tbody><tr><td align='center'>m</td> 
+            <td align='center'>Month as two digits</td>
+            <td align='center'>02</></tr>";
+ 
+      #row 5
+      echo "<tbody><tr><td align='center'>F</td> 
+            <td align='center'>Full month name</td>
+            <td align='center'>March</></tr>";
+ 
+      #row 6
+      echo "<tbody><tr><td align='center'>M</td> 
+            <td align='center'>Month name as three letters</td>
+            <td align='center'>Jun</></tr>";
+
+      #row 7
+      echo "<tbody><tr><td align='center'>Y</td> 
+            <td align='center'>Year as four digits</td>
+            <td align='center'>2020</></tr>";
+       #row 1
+      echo "<tbody><tr><td align='center'>Y</td> 
+            <td align='center'>Year as four digits</td>
+            <td align='center'>2020</></tr>";
+       #row 1
+      echo "<tbody><tr><td align='center'>Y</td> 
+            <td align='center'>Year as four digits</td>
+            <td align='center'>2020</></tr>";
+       #row 1
+      echo "<tbody><tr><td align='center'>Y</td> 
+            <td align='center'>Year as four digits</td>
+            <td align='center'>2020</></tr>";
+       #row 1
+      echo "<tbody><tr><td align='center'>Y</td> 
+            <td align='center'>Year as four digits</td>
+            <td align='center'>2020</></tr>";
+       #row 1
+      echo "<tbody><tr><td align='center'>Y</td> 
+            <td align='center'>Year as four digits</td>
+            <td align='center'>2020</></tr>";
+       #row 1
+      echo "<tbody><tr><td align='center'>Y</td> 
+            <td align='center'>Year as four digits</td>
+            <td align='center'>2020</></tr>";
+       #row 1
+      echo "<tbody><tr><td align='center'>Y</td> 
+            <td align='center'>Year as four digits</td>
+            <td align='center'>2020</></tr>";
+       #row 1
+      echo "<tbody><tr><td align='center'>Y</td> 
+            <td align='center'>Year as four digits</td>
+            <td align='center'>2020</></tr>";
+       #row 1
+      echo "<tbody><tr><td align='center'>Y</td> 
+            <td align='center'>Year as four digits</td>
+            <td align='center'>2020</></tr>";
+ 
+?>
+
+  </body>
+</html>
