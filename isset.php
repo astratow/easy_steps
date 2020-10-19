a9f60b7242d948433698849e6daa1da2f4240447
astratow <davno_pora@mail.com>
2020-10-19 05:51:01 +0100

    isset.php slightly changed upper cases on values isset_handler.php properly written and tested positive. isset.php OK

--- isset.php
+++ isset.php
@@ -9,9 +9,9 @@
     <form action="isset_handler.php" method="POST">
      <fieldset>
      <legend>What kind of language is PHP?</legend>
-    Scripting<input type="radio" name="definition" value="scripting"><br>
-    Markup<input type="radio" name="definition" value="markup"><br>
-    Programming<input type="radio" name="definition" value="programming"><br>
+    Scripting<input type="radio" name="definition" value="Scripting"><br>
+    Markup<input type="radio" name="definition" value="Markup"><br>
+    Programming<input type="radio" name="definition" value="Programming"><br>
     </fieldset><p><input type="submit" ></></form>
 
   </body>
--- isset_handler.php
+++ isset_handler.php
@@ -2,12 +2,27 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title> Basic PHP template </title>
+    <title>Checking set values - isset handler</title>
   </head>
   <body>
     <?php
       #write traditional greetings;
-      echo '<h1>Hello, World!</h1>';
+      #echo '<h1>Hello, World!</h1>';
+      if(isset($_POST['definition'])){
+        $definition=$_POST['definition'];
+      }else{
+        $definition=NULL;
+      }
+      #response statements to be inserted here
+      if($definition!=NULL){
+        if($definition!='Scripting'){
+          echo "$definition is incorrect";
+        }else{
+          echo "$definition is correct";
+        }
+      }else{
+        echo "You must select one answer";
+      }
     ?>
 
   </body>
