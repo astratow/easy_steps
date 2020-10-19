b4b9577d35ad17e74dde6b6cf6c7e2f660147cc3
astratow <davno_pora@mail.com>
2020-10-19 21:23:03 +0100

    valid_handler.php is a dummy now

--- valid_handler.php
+++ valid_handler.php
@@ -2,12 +2,12 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title> Basic PHP template </title>
+    <title> Valid Handler </title>
   </head>
   <body>
     <?php
       #write traditional greetings;
-      echo '<h1>Hello, World!</h1>';
+      echo '<h1>Hello, this is validation handler!</h1>';
     ?>
 
   </body>
