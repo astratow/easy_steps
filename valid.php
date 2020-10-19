ce2db8b5176473bdcd6712c507b4de6d9e99cc5e
astratow <davno_pora@mail.com>
2020-10-19 21:20:24 +0100

    valid.php created

---
+++ valid.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title> Basic PHP - Validating data form </title>
+  </head>
+  <body>
+    <?php
+      #write traditional greetings;
+      echo '<h1>Basic PHP - Validating data form </h1>';
+      ?>
+    <form action="valid_handler.php" method="POST">
+
+  </body>
+</html>
