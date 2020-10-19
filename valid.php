d7505a114d5528af35c60060853f7fb694f1cdda
astratow <davno_pora@mail.com>
2020-10-19 22:15:41 +0100

    valid.php is now working, valid_handler.php is still a dummy now

--- valid.php
+++ valid.php
@@ -10,6 +10,13 @@
       echo '<h1>Basic PHP - Validating data form </h1>';
       ?>
     <form action="valid_handler.php" method="POST">
+    <fieldset>
+    <legend> Enter a quantity and email address</legend>
+    <p>Quantity: <input type="text" name="quantity"></p>
+    <p>Email Address: <input type="text" name="email"></p>
+    </fieldset>
+    <p><input type="submit"></p>
+    </form>
 
   </body>
 </html>
