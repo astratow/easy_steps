991a2213af702d3cc0960c7cc54fedd19103b729
astratow <davno_pora@mail.com>
2020-10-20 05:34:47 +0100

    valid_handler.php is now working

--- valid_handler.php
+++ valid_handler.php
@@ -8,6 +8,33 @@
     <?php
       #write traditional greetings;
       echo '<h1>Hello, this is validation handler!</h1>';
+      if(!empty($_POST['quantity'])){
+        $quantity=$_POST['quantity'];
+        #format validation tp be inserted
+        if(!is_numeric($quantity)){
+          $quantity=NULL;
+          echo 'Quantity must be numeric<br>';
+        }
+      }else{
+        $quantity=NULL;
+        echo 'You must enter a quantity<br>';
+      }
+      if(!empty($_POST['email'])){
+        $email=$_POST['email'];
+        #format validation to be inserted here
+        $pattern="^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$^";
+        if(!preg_match($pattern, $email)){
+          $email=NULL;
+          echo 'Emal address is incorrect format';
+        }
+      }else{
+        $email=NULL;
+        echo 'Email address is incorrect format';
+      }
+      if(($quantity!=NULL)&&($email!=NULL)){
+        echo "Email: $email<br>Quantity: $quantity";
+
+      }
     ?>
 
   </body>
