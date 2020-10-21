a28f821bc2a8fa52b8baa50507160bfdb761f2a5
astratow <davno_pora@mail.com>
2020-10-21 04:49:03 +0100

    hidden.php acting strange...

--- hidden.php
+++ hidden.php
@@ -11,6 +11,21 @@
       date_default_timezone_set('UTC');
       $time=date('H:i, F j');
       $user='Artur';
+
+      echo '
+        <form action="hidden_handler.php method="POST">
+        <fieldset>
+        <legend>Send us your comments</legend>
+        <textarea rows="5" col="20" name="comment">
+        </textarea>
+        <input type="hidden" name="user" value="'.$user.'">
+        <input type="hidden" name="time" value="'.$time.'">
+        </fieldset>
+        <p>
+        <input type="submit">
+        </p>
+        </form>
+      ';
     ?>
 
   </body>
