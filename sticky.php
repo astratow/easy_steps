53b003119a0c084bdc5bc00baceaca9dbe152190
astratow <davno_pora@mail.com>
2020-10-22 05:48:51 +0100

    sticky.php form added

--- sticky.php
+++ sticky.php
@@ -2,13 +2,31 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title> Basic PHP template </title>
+    <title> Making sticky forms</title>
   </head>
   <body>
-    <?php
-      #write traditional greetings;
-      echo '<h1>Hello, World!</h1>';
+    
+  <h1>Sticky form</h1>
+  <form action="sticky.php" method="POST">
+    <p>Name:
+    <input type="text" name="name" 
+      value="<?php if(isset($_POST['name']))
+                  echo $_POST['name']
+                  ;?>">
+    </p>
+    <p>Email:
+      <input type="text" name="email" 
+            value="<?php if(isset($_POST['name']))
+            echo $_POST['email'];?>">
+    </p>
+    <p>
+      <input type="submit">
+    </p>
+    </form>
+    <?php 
+      if($_SERVER['REQUEST_METHOD']=='POST'){
+        #Statements to be inserted here
+      }
     ?>
-
   </body>
 </html>
