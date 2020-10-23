e906f5c9336d5bd5e363426ca21b3613ce7159c6
astratow <davno_pora@mail.com>
2020-10-23 05:12:54 +0100

    footer.php updated, corrected, up and running

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title> <?PHP echo $page_title;?> </title>
+    <link rel="stylesheet" href="includes/style.css">
+  </head>
+  <body>
+    <h1>Page Header</h1>
+
+  </body>
+</html>
