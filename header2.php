5271289692261b2faa44bd3fe277cf9d47de2090
astratow <davno_pora@mail.com>
2020-10-27 20:09:24 +0000

    header2.php tested and corrected

--- header2.php
+++ header2.php
@@ -6,9 +6,27 @@
   </head>
   <body>
     <?php
-      #write traditional greetings;
-      echo '<h1>Hello, World!</h1>';
+	if(isset($_POST['id'])){
+		$id=$_POST['id'];
+		#Handler statements to be inserted here
+		if($id==123){
+			session_start();
+			$_SESSION['id']=$id;
+			header('Location: location.php');
+			exit();
+		}else{
+			echo "<p>$id is an incorrect ID!</p>";
+		}
+		}
+	
+	echo '<form action="header2.php" method="POST">
+		<fieldset><legend>Enter Your User ID</legend>
+		<p>ID: <input type="text" name="id"></p>
+		</fieldset><input type="submit"></form>';
     ?>
 
   </body>
+</html>
+<!--
+# header() set Location header to redirect the browser to a new page 
 </html>
