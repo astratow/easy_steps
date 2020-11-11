1bf3658f149f4ab61d8bd90368a7ece00412a79b
astratow <davno_pora@mail.com>
2020-11-11 22:25:11 +0000

    update.php created and tested

---
+++ update.php
@@ -0,0 +1,17 @@
+<?php
+require('../connectDB.php');
+function show_records($dbc){
+  $query = 'SELECT * FROM watches';
+  $result = mysqli_query($dbc, $query);
+  $numRows = mysqli_num_rows($result);
+  if($numRows>0){
+      echo '<h1>Records in watches table</h1>';
+      while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+        echo '<br>' . $row['model'] . '|' . $row['style'] . '@' . $row['price'];
+      }
+    }else{
+      echo '<p>' . mysqli_error($dbc) . '</p>';
+    }
+  }
+  show_records($dbc);
+  ?>
