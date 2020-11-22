d3c840ccdadf3131b98bb2697ca730e26db7b55f
astratow <davno_pora@mail.com>
2020-11-22 04:47:37 +0000

    forum.php created and updated

--- forum.php
+++ forum.php
@@ -7,7 +7,46 @@
   <body>
     <?php
       #write traditional greetings;
-      echo '<h1>Hello, World!</h1>';
+      #step 1
+      session_start();
+      #step 2
+      if(!isset($_SESSION['user_id'])){
+        require('login_tools.php');
+        load();
+      }
+      #step 3
+      $page_title = 'Forum';
+      include('includes/header.php');
+      #step 4
+      require('../connectDB.php');
+      #step 5
+      $query = "SELECT * FROM forum";
+      $result = mysqli_query($dbc, $query);
+      if(mysqli_num_rows($result)>0) {
+        #statement
+        #step 6
+        echo '<table<tr><th>Posted By</th>
+          <th>Subject</th><th id="msg">Message</th></tr>';
+        while($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+          echo '<tr><td>' . $row['first_name'] . ' ' . $row['last_name'] .
+            '<br>' . $row['post_date'] . '</td></td>' .$row['subject'] . 
+            '</td><td>' . $row['message'] . '</td></tr>';
+        }
+        echo '</table>';
+      } else {
+        echo '<p>There are currently no messages.</p>';
+      }
+      #step 7
+      echo '<p>
+        <a href="post.php">Post Message</a> |
+        <a href="shop.php">Shop Message</a> |
+        <a href="home.php">Home Message</a> |
+        <a href="goodbye.php">Goodbye Message</a></p>';
+      #step 8
+      mysqli_close($dbc);
+      #step 9
+      include('includes/footer.php');
+
     ?>
 
   </body>
