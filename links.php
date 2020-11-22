4f5aab37b7850e48714ac49267a88eccfabf1215
astratow <davno_pora@mail.com>
2020-11-22 06:18:06 +0000

    links.php created. it stores links that repeat on pages

---
+++ links.php
@@ -0,0 +1,7 @@
+<?php
+$links = '<p>
+        <a href="post.php">Post Message</a> |
+        <a href="shop.php">Shop Message</a> |
+        <a href="home.php">Home Message</a> |
+        <a href="goodbye.php">Goodbye Message</a></p>';
+?>
