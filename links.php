633e925863a772e1a6d170719c2277388fd8507a
astratow <davno_pora@mail.com>
2020-11-23 04:56:47 +0000

    link.php updated

--- links.php
+++ links.php
@@ -1,7 +1,7 @@
 <?php
 $links = '<p>
         <a href="post.php">Post Message</a> |
-        <a href="shop.php">Shop Message</a> |
-        <a href="home.php">Home Message</a> |
-        <a href="goodbye.php">Goodbye Message</a></p>';
+        <a href="shop.php">Shop</a> |
+        <a href="home.php">Home</a> |
+        <a href="goodbye.php">Goodbye</a></p>';
 ?>
