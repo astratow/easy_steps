3a1ba438daae591d5642bd43e5e15099273994f5
astratow <davno_pora@mail.com>
2020-12-04 05:11:10 +0000

    links.php improved

--- links.php
+++ links.php
@@ -1,9 +1,9 @@
 <?php
-$post 	= 	'<a href="post.php">Post Message</a>|';
-$shop 	= 	'<a href="shop.php">Shop</a> |';
-$cart 	= 	'<a href="cart.php">Cart</a> |';
-$forum 	= 	'<a href="forum.php">Forum</a>|';
-$home 	= 	'<a href="home.php">Home</a> |';
+$post 	= 	'<a href="post.php">Post Message</a> | ';
+$shop 	= 	'<a href="shop.php">Shop</a> | ';
+$cart 	= 	'<a href="cart.php">Cart</a> | ';
+$forum 	= 	'<a href="forum.php">Forum</a> | ';
+$home 	= 	'<a href="home.php">Home</a> | ';
 $logout =  	'<a href="goodbye.php">Goodbye</a>';
 
 ?>
