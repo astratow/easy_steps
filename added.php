1c64a14536fd07622a70b777b37e85fe0b57c325
astratow <davno_pora@mail.com>
2020-12-05 06:28:16 +0000

    added.php fixed

--- added.php
+++ added.php
@@ -11,17 +11,18 @@ require('../connectDB.php');
 $query = "SELECT * FROM shop WHERE item_id = $id";
 $result = mysqli_query($dbc, $query);
 if(mysqli_num_rows($result) == 1){
-  $row = mysqli_fetch_array($row, MYSQLI_ASSOC);
+  $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
   #step7 s2 268
   if(isset($_SESSION['cart'][$id])){
     $_SESSION['cart'][$id]['quantity']++;
-    echo '<p>Another ' . $row["item_name"]. 'has been added to you cart';
+    echo '<p>Another ' . $row["item_name"]. ' has been added to you cart';
   }else{
     $_SESSION['cart'][$id] = array('quantity' => $row['item_price']);
     echo '<p> A ' . $row["item_name"] . 'has been added to your cart</p>';
   }
 }
 mysqli_close($dbc);
+require('links.php');
 echo '<p> ' . $shop . $cart . $forum . $home . $logout . '</p>';
 include('includes/footer.php');
 ?>
