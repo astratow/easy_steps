269da6e77fbdbad48e41fe4313dcfa51c16d66e4
astratow <davno_pora@mail.com>
2020-12-09 05:20:07 +0000

    checkout.php created, errors on

--- checkout.php
+++ checkout.php
@@ -16,9 +16,34 @@
       include('includes/header.php');
       if(isset($_GET['total'])&&($_GET['total']>0)&&(!empty($_SESSION['cart']))){
         #step 5-10
+        require('../connectDB.php');
+        $query = "INSERT INTO orders(user_id, total, order_date) VALUES("
+          . $_SESSION['user_id'] . "," .
+          $_GET['total'] . ", NOW())";
+        $result = mysqli_query($dbc, $query);
+        $order_id = mysqli_insert_id($dbc);
+        $query = "SELECT * FROM shop WHERE item_id IN (";
+        foreach($_SESSION['cart'] as $id => $value){
+          $query =$id . ',';
+        }
+        $query = substr($query, 0, -1) . ') ORDER BY item_id ASC';
+        $result = mysqli_query($dbc, $query);
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+          $query_W = "INSERT INTO order_contents (order_id, item_id, quantity, price) 
+                      VALUES($order_id, " . $row['item_id'] . "," .
+                      $_SESSION['cart'][$row['item_id']]['quantity'] . "," .
+                      $_SESSION['cart'][$row['item_id']]['price'] . ")";
+          $result_w = mysqli_query($dbc, $query_w);
+          mysqli_close($dbc);
+          echo "<p>Thanks for your order. Your Order Number is #" . $order_id . "</p>";
+          $_SESSION['cart'] = NULL;
+        }  
       }else{
         echo '<p>There are no items in your cart.</p>';
       }
+      require('links.php');
+      echo '<p>' . $shop . $forum . $home . $logout. '</p>';
+      include('includes/footer.php');
     ?>
 
   </body>
